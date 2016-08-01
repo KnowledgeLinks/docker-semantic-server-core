1aad4c130c76449f36a9f36e79a1c5b8cc23f534
Jeremy Nelson <jermnelson@gmail.com>
2016-08-01 09:30:47 -0600

    modified seten.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="$JAVA_OPTS -Dtriplestore.baseUrl=localhost:8080/bigdata/sparql  -Dfcrepo.home=/usr/share/fcrepo4-data -Devent.baseUri=https://www.coloradocollege.edu/library -Dfcrepo.baseUrl=localhost:8080/fedora/rest"
+export JAVA_OPTS="$JAVA_OPTS -Dtriplestore.baseUrl=localhost:8080/bigdata/sparql  -Dfcrepo.home=/usr/share/fcrepo4-data -Devent.baseUri=https://www.coloradocollege.edu/library -Dfcrepo.baseUrl=localhost:8080/fedora/rest -Dfcrepo.modshape.configuration=config"
 export CATALINA_OPTS="-Dcom.sun.management.jmxremote"
